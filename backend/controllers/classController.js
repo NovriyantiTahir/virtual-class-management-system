3878edc2091937eca2f0b87c56fa1a0147ae6ba3
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-30 21:28:26 +0800

    Update classController.js

--- backend/controllers/classController.js
+++ backend/controllers/classController.js
@@ -1,1 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Membuat skema untuk Class
+const ClassSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  teacher: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
+  students: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    }
+  ],
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('class', ClassSchema);
 
