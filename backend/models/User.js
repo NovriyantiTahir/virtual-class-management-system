af9d8a3d46bd6b3596dc6792275e3ff88ea2d371
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-30 21:36:41 +0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Membuat skema untuk User
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['student', 'teacher', 'admin'],
+    default: 'student'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
 
