6bd7801bb8039351c71e187e9fdbaae71f73ed68
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-30 21:18:21 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,29 @@
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Tentukan URI MongoDB
+const dbURI = 'mongodb://localhost:27017/nama_database';
+
+// Konfigurasi opsi koneksi
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+};
+
+// Fungsi untuk menghubungkan ke database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI, options);
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Keluar dari proses dengan gagal
+    process.exit(1);
+  }
+};
+
+// Ekspor fungsi connectDB
+module.exports = connectDB;
 
