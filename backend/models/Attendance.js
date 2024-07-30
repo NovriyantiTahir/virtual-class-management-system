d4a1401d3bcaeb2afe03820bdccdba5ceebb586e
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-30 21:34:29 +0800

    Update Attendance.js

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Membuat skema untuk Attendance
+const AttendanceSchema = new Schema({
+  class: {
+    type: Schema.Types.ObjectId,
+    ref: 'class',
+    required: true
+  },
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['Present', 'Absent', 'Late', 'Excused'],
+    required: true
+  },
+  remarks: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('attendance', AttendanceSchema);
 
