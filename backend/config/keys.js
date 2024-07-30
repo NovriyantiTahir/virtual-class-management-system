79b832d4cd7934b4ea7d05241f1f99427a79c193
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-30 21:20:11 +0800

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,1 +1,16 @@
+// keys.js
+
+if (process.env.NODE_ENV === 'production') {
+  // Pada lingkungan produksi, gunakan variabel lingkungan yang sesuai
+  module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
+  };
+} else {
+  // Pada lingkungan pengembangan, gunakan variabel lingkungan lokal
+  module.exports = {
+    mongoURI: 'mongodb://localhost:27017/nama_database',
+    secretOrKey: 'your_secret_key'
+  };
+}
 
