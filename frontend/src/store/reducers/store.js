9891741201cd789f1aa2134a79f12354762e10c9
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-31 19:28:31 +0800

    Update store.js

--- frontend/src/store/reducers/store.js
+++ frontend/src/store/reducers/store.js
@@ -1,1 +1,22 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import userReducer from './userReducer';
+import classReducer from './classReducer';
+import attendanceReducer from './attendanceReducer';
+
+// Combine all reducers
+const rootReducer = combineReducers({
+    users: userReducer,
+    classes: classReducer,
+    attendance: attendanceReducer
+});
+
+// Create store with middleware and Redux DevTools extension
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
 
