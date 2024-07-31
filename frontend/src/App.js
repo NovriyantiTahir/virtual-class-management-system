4ce7b86b342ae6fc8b026ba30e2bc7f93b7f9614
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-31 19:34:53 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
+import ClassPage from './pages/ClassPage';
+import AttendancePage from './pages/AttendancePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <Router>
+                <div>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={HomePage} />
+                        <Route path="/users" component={UserPage} />
+                        <Route path="/classes" component={ClassPage} />
+                        <Route path="/attendance" component={AttendancePage} />
+                        <Route component={NotFoundPage} />
+                    </Switch>
+                </div>
+            </Router>
+        </Provider>
+    );
+}
+
+export default App;
 
