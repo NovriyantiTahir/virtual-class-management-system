d576c2464ec021614bdd48faad2e6a359a351539
NovriyantiTahir <novriantitahir@gmail.com>
2024-07-31 19:30:13 +0800

    Update userReducer.js

--- frontend/src/store/reducers/userReducer.js
+++ frontend/src/store/reducers/userReducer.js
@@ -1,1 +1,51 @@
+// Action types
+const ADD_USER = 'ADD_USER';
+const REMOVE_USER = 'REMOVE_USER';
+const SET_USERS = 'SET_USERS';
+
+// Initial state
+const initialState = {
+    userList: []
+};
+
+// Reducer function
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USERS:
+            return {
+                ...state,
+                userList: action.payload
+            };
+        case ADD_USER:
+            return {
+                ...state,
+                userList: [...state.userList, action.payload]
+            };
+        case REMOVE_USER:
+            return {
+                ...state,
+                userList: state.userList.filter(user => user !== action.payload)
+            };
+        default:
+            return state;
+    }
+};
+
+// Action creators
+export const setUsers = (userList) => ({
+    type: SET_USERS,
+    payload: userList
+});
+
+export const addUser = (userName) => ({
+    type: ADD_USER,
+    payload: userName
+});
+
+export const removeUser = (userName) => ({
+    type: REMOVE_USER,
+    payload: userName
+});
+
+export default userReducer;
 
